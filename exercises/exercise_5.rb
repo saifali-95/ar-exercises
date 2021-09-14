c2a94e96e2c47f53b839c09cb7b46b23c06fb6af
Saif Ali <saifali7646@gmail.com>
2021-09-14 21:49:25 +0000

    total revenue and average revenue all stores and count of store with over 1M revenue

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@total_revenue = Store.sum(:annual_revenue)
+puts "Total Revenue #{@total_revenue}"
+
+@average_revenue = @total_revenue / Store.count
+puts "Average Revenue of all stores #{@average_revenue}"
+
+@store_1M = (Store.where(["annual_revenue >= ?", 1000000])).count
+
+puts "Total store counts with over 1M annual_revenue #{@store_1M}"
