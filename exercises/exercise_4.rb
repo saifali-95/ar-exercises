61a7dd510f4f3f834fb8971794b537a5fa09b2a0
Saif Ali <saifali7646@gmail.com>
2021-09-14 22:03:46 +0000

    fix bug

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,26 +7,26 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
-store4 = Store.new
-store4.name = "Surrey"
-store4.annual_revenue = 224000
-store4.mens_apparel = false
-store4.womens_apparel = true
-store4.save
+@store4 = Store.new
+@store4.name = "Surrey"
+@store4.annual_revenue = 224000
+@store4.mens_apparel = false
+@store4.womens_apparel = true
+@store4.save
 
-store5 = Store.new
-store5.name = "Whistler"
-store5.annual_revenue = 1900000
-store5.mens_apparel = true
-store5.womens_apparel = false
-store5.save
+@store5 = Store.new
+@store5.name = "Whistler"
+@store5.annual_revenue = 1900000
+@store5.mens_apparel = true
+@store5.womens_apparel = false
+@store5.save
 
-store6 = Store.new
-store6.name = "Yaletown"
-store6.annual_revenue = 430000
-store6.mens_apparel = true
-store6.womens_apparel = true
-store6.save
+@store6 = Store.new
+@store6.name = "Yaletown"
+@store6.annual_revenue = 430000
+@store6.mens_apparel = true
+@store6.womens_apparel = true
+@store6.save
 
 @mens_stores = Store.where(["mens_apparel = ? and womens_apparel = ?", true, false])
 
